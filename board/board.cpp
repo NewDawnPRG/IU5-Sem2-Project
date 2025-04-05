58da5330ba8edca067d4c9c72d08374fb6fc7231
Yarik <chessiu5bmstu@gmail.com>
2025-04-05 15:20:58 +0300

    Added some functions

--- board/board.cpp
+++ board/board.cpp
@@ -1,17 +1,16 @@
 #include "board.h"
 
-Board::Board(){
+Board::Board() {
     const int initData[9][9] = {
-        {-4,-5,-6,-7,-8,-7,-6,-5,-4},
-        {0,-3,0,0,0,0,0,-2,0},
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0},
-        {1,1,1,1,1,1,1,1,1},
-        {0,2,0,0,0,0,0,3,0},
-        {4,5,6,7,8,7,6,5,4}
-    }; // создаем массив с текущими фигурами на доске
+            {-4, -5, -6, -7, -8, -7, -6, -5, -4},
+            {0,  -3, 0,  0,  0,  0,  0,  -2, 0},
+            {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+            {0,  0,  0,  0,  0,  0,  0,  0,  0},
+            {0,  0,  0,  0,  0,  0,  0,  0,  0},
+            {0,  0,  0,  0,  0,  0,  0,  0,  0},
+            {1,  1,  1,  1,  1,  1,  1,  1,  1},
+            {0,  2,  0,  0,  0,  0,  0,  3,  0},
+            {4,  5,  6,  7,  8,  7,  6,  5,  4}}; // создаем массив с текущими фигурами на доске
 
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
@@ -29,27 +28,27 @@ Board::Board(){
     checkWhite_ = false; // установки шахов
 };
 
-void Board::SetMove(int oldInd1, int oldInd2, int newInd1, int newInd2){
-    if(boardMas_[newInd1][newInd2] != 0) {// если в новой позиции была фигура
-        capturedAdd(boardMas_[newInd1][newInd2]);// отправить фигуру в список
+void Board::SetMove(int oldInd1, int oldInd2, int newInd1, int newInd2) {
+    if (boardMas_[newInd1][newInd2] !=
+        0) {                                             // если в новой позиции была фигура
+        capturedAdd(boardMas_[newInd1][newInd2]); // отправить фигуру в список
     }
-    boardMas_[newInd1][newInd2] = boardMas_[oldInd1][oldInd2];// установка фигуры на позицию
-    boardMas_[oldInd1][oldInd2] = 0; // обнуление предыдущей позиции
-    CheckCheck();// проверить, не появился ли шах на доске
-    currentMove_ = !currentMove_;// сменить ход
+    boardMas_[newInd1][newInd2] = boardMas_[oldInd1][oldInd2]; // установка фигуры на позицию
+    boardMas_[oldInd1][oldInd2] = 0;                           // обнуление предыдущей позиции
+    CheckCheck();                                              // проверить, не появился ли шах на доске
+    currentMove_ = !currentMove_;                              // сменить ход
 };
 
-void Board::capturedAdd(int figure){
-    if(currentMove_){ // если ход черных
+void Board::capturedAdd(int figure) {
+    if (currentMove_) { // если ход черных
         int i = 0;
-        while (capturedWhite_[i]!=0){
+        while (capturedWhite_[i] != 0) {
             i++;
         }
         capturedWhite_[i] = figure * -1; // добавим в список захваченных белых фигур еще одну
-    }
-    else{ // иначе
+    } else { // иначе
         int i = 0;
-        while (capturedBlack_[i]!=0){
+        while (capturedBlack_[i] != 0) {
             i++;
         }
         capturedBlack_[i] = figure * -1; // добавим в список захваченных черных фигур еще одну
@@ -60,42 +59,51 @@ int Board::GetCell(int ind1, int ind2) const {
     return boardMas_[ind1][ind2];
 };
 
-void Board::capturedRemove(int ind1, int ind2, int num){
-    if(boardMas_[ind1][ind2] == 0){ // если в точке нету фигур
-        if(currentMove_){ // если ход черных
+void Board::capturedRemove(int ind1, int ind2, int num) {
+    if (boardMas_[ind1][ind2] == 0) { // если в точке нету фигур
+        if (currentMove_) {                                                 // если ход черных
             boardMas_[ind1][ind2] == capturedWhite_[num]; // выбрасиываем фигуру
-            capturedWhite_[num] = 0; // ставим на это место 0
-        }
-        else{ // иначе
+            capturedWhite_[num] = 0;                      // ставим на это место 0
+        } else {                                                 // иначе
             boardMas_[ind1][ind2] == capturedBlack_[num]; // выбрасиываем фигуру
-            capturedBlack_[num] = 0; // ставим на это место 0
+            capturedBlack_[num] = 0;                      // ставим на это место 0
         }
     }
-    CheckCheck();// проверить, не появился ли шах на доске
-    currentMove_ = !currentMove_;// сменить ход
+    CheckCheck();                 // проверить, не появился ли шах на доске
+    currentMove_ = !currentMove_; // сменить ход
 }
 
-void Board::CheckCheck(){
+void Board::CheckCheck() {
     int ind1;
     int ind2;
     FindKing(ind1, ind2);
-    if(currentMove_){ // если ход черных
-        if(ind2+1 <10){
-            if((boardMas_[ind1][ind2+1] == 1) ||(boardMas_[ind1][ind2+1] == 3) ||(boardMas_[ind1][ind2+1] == 4) ||(boardMas_[ind1][ind2+1] == 6) ||(boardMas_[ind1][ind2+1] == 7)){
+    if (currentMove_) { // если ход черных
+        if (ind2 + 1 < 10) {
+            if ((boardMas_[ind1][ind2 + 1] == 1) || (boardMas_[ind1][ind2 + 1] == 3) ||
+                (boardMas_[ind1][ind2 + 1] == 4) || (boardMas_[ind1][ind2 + 1] == 6) ||
+                (boardMas_[ind1][ind2 + 1] == 7)) {
                 checkWhite_ = true;
             }
         }
-    }
-    else{
-
+    } else {
     }
 }
 
-void Board::FindKing(int &ind1, int &ind2){
-    if(currentMove_){ // если ход черных
+void Board::FindKing(int &ind1, int &ind2) {
+    if (currentMove_) { // если ход черных
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
-                if(boardMas_[i][j] == 8) {// если король найден
+                if (boardMas_[i][j] == 8) { // если король найден
+                    ind1 = i;
+                    ind2 = j;
+                    break;
+                }
+            }
+        }
+    } else {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (boardMas_[i][j] == -8) { // если король найден
                     ind1 = i;
                     ind2 = j;
                     break;
@@ -103,15 +111,16 @@ void Board::FindKing(int &ind1, int &ind2){
             }
         }
     }
-    else{
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if(boardMas_[i][j] == -8) {// если король найден
-                    ind1 = i;
-                    ind2 = j;
-                    break;
-                }
-            }
-        }
-    }
+}
+
+bool Board::getCurrentMove() {
+    return currentMove_;
+}
+
+int *Board::getCapturedBlack() {
+    return capturedBlack_;
+}
+
+int *Board::getCapturedWhite() {
+    return capturedWhite_;
 }
--- board/board.h
+++ board/board.h
@@ -3,23 +3,26 @@
 
 class Board {
 public:
-    Board(); // Конструктор доски
-    ~Board();// Деструктор доски
+    Board();  // Конструктор доски
+    ~Board(); // Деструктор доски
 
-    void SetMove(int oldInd1, int oldInd2, int newInd1, int newInd2);// Выполнить ход
-    void capturedAdd(int figure);// добавить фигуру в список
-    void capturedRemove(int ind1, int ind2, int num);// убрать фигуру из списка
-    int GetCell(int ind1, int ind2) const;// Получить информацию о клетке поля
-    void CheckCheck();// проверка наличия шаха
-    void FindKing(int &ind1, int &ind2); // поиск короля
+    void SetMove(int oldInd1, int oldInd2, int newInd1, int newInd2); // Выполнить ход
+    void capturedAdd(int figure);                                     // добавить фигуру в список
+    void capturedRemove(int ind1, int ind2, int num);                 // убрать фигуру из списка
+    int GetCell(int ind1, int ind2) const;                            // Получить информацию о клетке поля
+    void CheckCheck();                                                // проверка наличия шаха
+    void FindKing(int &ind1, int &ind2);                              // поиск короля
+    bool getCurrentMove();                                            // Получить текущий ход
+    int *getCapturedBlack();                                          // Получить массив захваченных черных фигур
+    int *getCapturedWhite();                                          // Получить массив захваченных белых фигур
 
 private:
-    int boardMas_[9][9]; // Массив самой доски
-    int capturedBlack_[19];// Массив захваченных черных фигур
-    int capturedWhite_[19];// Массив захваченных белых фигур
-    bool currentMove_; // Чей текущий ход?
-    bool checkBlack_; // Находится ли король черных под шахом?
-    bool checkWhite_;// Находится ли король белых под шахом?
+    int boardMas_[9][9];    // Массив самой доски
+    int capturedBlack_[19]; // Массив захваченных черных фигур
+    int capturedWhite_[19]; // Массив захваченных белых фигур
+    bool currentMove_;      // Чей текущий ход?
+    bool checkBlack_;       // Находится ли король черных под шахом?
+    bool checkWhite_;       // Находится ли король белых под шахом?
 };
 
-#endif //BOARD_H
+#endif // BOARD_H
